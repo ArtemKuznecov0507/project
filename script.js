8f3d889c4d6ef052a726837cfbbc40b5fa582e14
Artem <artemkuznecov0507@gmail.com>
2022-02-16 18:14:38 +0300

    practice#4 done

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 "use strict";
 let numbersOfFilms; 
-function start (){
-    numbersOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", '');
-    while ( numbersOfFilms == '' || numbersOfFilms == null || isNaN(numbersOfFilms)){
-        numbersOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", " "); 
-    }
-    
-}
-   start();
-
 
 const personalMovieDB = {
     count: numbersOfFilms,
@@ -16,56 +7,83 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false,
+    start : function(){
+        numbersOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", '');
+        while ( numbersOfFilms == '' || numbersOfFilms == null || isNaN(numbersOfFilms)){
+            numbersOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", " "); 
+        }
+        personalMovieDB.count = numbersOfFilms;
+        
+    },
+    rememberMyFilms : function(){
+        for ( let i = 0; i < 2; i++){
+            const a = prompt ("Один из последних просмотренных фильмов?", "");
+            const b = prompt ("На сколько оцените его?", ""); 
+           
+           if ( a != null && b != null && a != '' && b != '' && a.length < 50){
+               console.log( 'Done');
+               personalMovieDB.movie [a] = b;
+           } else {
+               console.log( 'Error');
+               i--;
+           }
+        } 
+    },
+    detectPersonalLevel : function(){
+        if (personalMovieDB.count < 10){
+            console.log( 'Просмотрено довольно мало фильмов!');
+        } else if (10 <= personalMovieDB.count < 30){
+            console.log( 'Вы классический зритель');
+        } else if (personalMovieDB.count >= 30){
+            console.log( 'Вы киноман!');
+        } else {
+            console.log( 'Произошла ошибка!');
+        }
+        
+    },
+    showMyDB : function(){
+        if (personalMovieDB.privat == false){
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres : function(){
+        for ( let i = 1; i < 4; i++){
+            let genre = prompt ( `Ваш любимый жанр под номером ${ i }`);
+            if (genre == undefined || genre == ''){
+                console.log('Вы ввели не корректное значение!Повторите попытку!');
+                 i--;
+            } else {
+                personalMovieDB.genres[ i-1 ] = genre;
+            }
+           
+        }
+        personalMovieDB.genres.forEach(( item, i ) => {
+            console.log( `Любимый жанр #${i + 1} - это ${personalMovieDB.genres[ i ]}` );
+
+        });
+        
+    },
+    toggleVisibleMyDB : function(){
+        if(personalMovieDB.privat ){
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
+    },
 };
 
 
-function rememberMyFilms (){
-    for ( let i = 0; i < 2; i++){
-        const a = prompt ("Один из последних просмотренных фильмов?", "");
-        const b = prompt ("На сколько оцените его?", ""); 
-       
-       if ( a != null && b != null && a != '' && b != '' && a.length < 50){
-           console.log( 'Done');
-           personalMovieDB.movie [a] = b;
-       } else {
-           console.log( 'Error');
-           i--;
-       }
-    } 
-}
-
-rememberMyFilms();
 
 
 
-function detectPersonalLevel (){
-    if (personalMovieDB.count < 10){
-        console.log( 'Просмотрено довольно мало фильмов!');
-    } else if (10 <= personalMovieDB.count < 30){
-        console.log( 'Вы классический зритель');
-    } else if (personalMovieDB.count >= 30){
-        console.log( 'Вы киноман!');
-    } else {
-        console.log( 'Произошла ошибка!');
-    }
-    
-}
-detectPersonalLevel();
 
-function showMyDB (){
-    if (personalMovieDB.privat == false){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB();
+
+
+
    
-   function writeYourGenres (){
-       for ( let i = 1; i < 4; i++){
-           personalMovieDB.genres [ i-1 ] = prompt ( `Ваш любимый жанр под номером ${ i }`);
-       }
-   }
-writeYourGenres();
-// переход на ветку main
+ 
+
+ 
 
 
 
