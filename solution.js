23a73c2bc3de1af90b4bd4cf56ecc47bedf272dc
Artem <artemkuznecov0507@gmail.com>
2022-02-18 18:28:24 +0300

    function reversStr & maxValue

--- solution.js
+++ solution.js
@@ -1,9 +1,24 @@
 //Ваша задача - создать функцию, которая может принимать любое неотрицательное целое число 
 //в качестве аргумента и возвращать его с цифрами в порядке убывания.
 // По сути, переставьте цифры, чтобы создать максимально возможное число.
-let numbers = [4,5,9,6,1,2];
-numbers.sort (function (a , b){
-    return a - b;
-});
-numbers = numbers.reverse();
-console.log(numbers);
+'use strict';
+function maxValue(numbers){
+  numbers = String(numbers);
+  numbers = numbers.split('').sort().reverse().join('');
+  numbers = Number(numbers);
+
+   console.log(typeof(numbers));
+  }
+
+maxValue(4567);
+
+
+
+// Создать функцию, которая будет принимать строку,
+// а возвращать ее в зеркальном виде.
+
+function reverseStr(text) {
+  text = text.split('').reverse().join('');
+  console.log(text);
+}
+reverseStr('hello');
